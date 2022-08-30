358a22c9ab12a1725529131d70b30666ac012e0b
Prabhu Suriya Kumar <prabhu.fullstack@gmail.com>
2022-08-30 09:36:41 +0530

    api for reports

--- cal-track-api/src/report.controller.ts
+++ cal-track-api/src/report.controller.ts
@@ -28,4 +28,39 @@ export class ReportController {
     }));
     return result;
   }
+
+  @Get('useraggregate')
+  async getAverageCalories(@UserInfo() user: User) {
+    const data = await this.foodService.getAll();
+    const dateLimit = getDate(-7);
+    console.log(dateLimit);
+    const groupedData: any = data
+      .filter((f) => new Date(f.consumedTime) > dateLimit)
+      .reduce((a: any, c: any) => {
+        if (a[c.userId]) {
+          a[c.userId].count++;
+          a[c.userId].sum += c.calories;
+        } else {
+          a[c.userId] = { count: 1, sum: c.calories, name: c.user.name };
+        }
+        return a;
+      }, {});
+    const result = Object.entries(groupedData).map(([id, values]: any) => ({
+      avg: values.sum / values.count,
+      sum: values.sum,
+      count: values.count,
+      userId: id,
+      userName: values.name,
+    }));
+
+    return result;
+
+    // return await this.foodService.getAvgCaloriesByUser();
+  }
+}
+
+function getDate(daysToAdd: number = 0) {
+  var d = new Date();
+  d.setDate(d.getDate() + daysToAdd);
+  return d;
 }
--- cal-track-api/src/services/food.service.ts
+++ cal-track-api/src/services/food.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@nestjs/common';
-import { Food, Prisma } from '@prisma/client';
+import { Food, prisma, Prisma } from '@prisma/client';
 import { DBService } from './database.service';
 
 @Injectable()
@@ -58,4 +58,46 @@ export class FoodService {
       where: { id },
     });
   }
+
+  // async getAvgCaloriesByUser(): Promise<any> {
+  //   // return this.dbService.food.groupBy({
+  //   //   by: ['userId'],
+  //   //   _avg: {
+  //   //     calories: true,
+  //   //   },
+  //   //   where: {
+  //   //     consumedTime: {
+  //   //       // lte: new Date(),
+  //   //       gte: new Date(new Date().setDate(-7)),
+  //   //     },
+  //   //   },
+  //   //   orderBy: [{ userId: 'asc' }],
+  //   // });
+  //   console.log(new Date(new Date().setDate(-7)));
+  //   return this.dbService.food.findMany({
+  //     // by: ['userId'],
+  //     // _avg: {
+  //     //   calories: true,
+  //     // },
+  //     where: {
+  //       consumedTime: {
+  //         // lte: new Date(),
+  //         gt: new Date(new Date().setDate(-7)),
+  //       },
+  //     },
+  //     // orderBy: [{ userId: 'asc' }],
+  //   });
+  // }
+
+  // async getAvgCaloriesByUser(): Promise<any> {
+  //   // return prisma.$query this.dbService.$queryRaw(`SELECT * FROM Food`);
+
+  //   // const startDate = new Date(new Date().setDate(-));
+  //   // const endDate = new Date();
+  //   return await this.dbService.$queryRaw(Prisma.sql`
+  //   SELECT *
+  //   FROM Food
+  //    -- WHERE strftime('%Y%m%d', consumedTime) > '20220820'
+  //   `);
+  // }
 }
