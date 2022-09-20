167a913f13b0a40a47098461e1d5d48ada64cbf8
Prabhu Suriya <prabhusuriya.chennai@gmail.com>
2022-09-20 18:28:18 +0530

    update prod env

--- cal-track/src/environments/environment.prod.ts
+++ cal-track/src/environments/environment.prod.ts
@@ -1,4 +1,7 @@
 export const environment = {
   API_BASE: 'https://calorie-tracker-production.up.railway.app',
+  NUTRITIONIX_APPID: '3c55f6bc',
+  NUTRITIONIX_APIKEY: '7815d1acef191463f784c1d6b2df5f81',
+  NUTRITIONIX_BASE: 'https://trackapi.nutritionix.com',
   production: true,
 };
